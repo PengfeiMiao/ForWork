8984d7961bcd4812edf3fffa76f8265e943d751a
PengfeiMiao <2018224980@qq.com>
2021-02-22 19:36:02 +0800

    模块调整

--- website/src/main/resources/static/js/index.js
+++ website/src/main/resources/static/js/index.js
@@ -1,14 +1,29 @@
-$(document).ready(function(){
-    $('#v0').css('display', 'block');
+$(document).ready(function () {
 })
 
-function show(name) {
-    let arr = ['v0', 'v1', 'v2', 'v3'];
-    arr.forEach(item => {
-        if (item === name) {
-            $('#' + item).css('display', 'block');
-        } else {
-            $('#' + item).css('display', 'none');
+function test() {
+
+    let columns = [], column = {}, param = {};
+    column["cond"] = 0;
+    column["name"] = "custom_code";
+    column["value"] = "18629330003";
+    columns.push(column);
+    param["columns"] = columns;
+    param["limit"] = 10;
+    param["offset"] = 0;
+
+    $.ajax({
+        url: "http://49.235.229.66/server/v1/console/base/data/red_black_list_info/list",
+        type: "post",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(param),
+        timeout: 6000,
+        success: function (res) {
+            console.log(res);
+        },
+        error: function (res) {
+            console.log("error: ", res);
         }
-    });
+    })
 }
